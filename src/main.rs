1feb7556b73b3309ec7f66f84977fbe1a4fed84a
Victor Berger <victor.berger@m4x.org>
2014-10-21 11:36:35 +0200

    Add `--cfg <file>` option and update dependencies.

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,16 @@
 
 #![feature(macro_rules,if_let,while_let)]
 
+extern crate argparse;
 extern crate time;
 extern crate toml;
 extern crate uuid;
 
 use std::io::Listener;
 use std::io::net::tcp::TcpListener;
+use std::os::set_exit_status;
+
+use argparse::{ArgumentParser, Store};
 
 pub mod channels;
 pub mod conf;
@@ -23,20 +27,34 @@ fn main() {
     //
     // CONFIG
     //
-    let configfile = from_str::<Path>("./metallirc.toml").unwrap();
+
+    let mut cfg_path = "./metallirc.toml".to_string();
+
+    let mut ap = ArgumentParser::new();
+    ap.set_description("metallircd");
+    ap.refer(&mut cfg_path).add_option(["--cfg"], box Store::<String>, "config file");
+    match ap.parse_args() {
+        Ok(()) => {}
+        Err(x) => { set_exit_status(x); return; }
+    }
+
+    let configfile = match from_str::<Path>(cfg_path.as_slice()) {
+        Some(path) => path,
+        None => { println!("Invalid path for config file."); set_exit_status(1); return }
+    };
     let serverconfig = match conf::load_config(configfile) {
         Ok(c) => c,
-        Err(e) => {println!("{}", e); return}
+        Err(e) => { println!("{}", e); set_exit_status(1); return }
     };
 
     // new clients handler
     let listener = match TcpListener::bind(serverconfig.address.as_slice(), serverconfig.port) {
         Ok(l) => l,
-        Err(e) => {println!("Could not bind port: {}", e); return}
+        Err(e) => { println!("Could not bind port: {}", e); set_exit_status(1); return }
     };
     let acceptor = match listener.listen() {
         Ok(a) => a,
-        Err(e) => {println!("Could not bind port: {}", e); return}
+        Err(e) => { println!("Could not bind port: {}", e); set_exit_status(1); return }
     };
 
     //
